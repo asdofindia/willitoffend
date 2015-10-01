8d6486cc33c014584cd1ba4f587ed92efcc08774
Tripad <tripad.mishra@gmail.com>
2015-10-02 05:20:14 +0530

    Commit 1

--- js/main.js
+++ js/main.js
@@ -1,1 +1,44 @@
 
+$(document).ready(function() {
+
+	function doneloading(){
+		$('.donemsg').delay(200).fadeIn();
+		setTimeout(function(){
+			$(".progressbarcnt").fadeOut();
+			$(".resultcnt").fadeIn("Slow");
+			$(".yes").addClass("growtext");
+			$(".resultcnt>p").delay(900).fadeIn("Slow");
+			$(".footer").delay(1400).animate({opacity: 1},500); 
+	        },1500);
+        }
+	$(".analyse").on("click",function(e){
+		e.preventDefault();
+		 $( ".mainform" ).fadeOut("slow");
+		$(".progressbarcnt").delay(200).fadeIn("fast");
+
+		var progressbar = $('#progressbar'),
+			max = progressbar.attr('max'),
+			time = (1500/max)*5,	
+		value = progressbar.val();
+
+		var loading = function() {
+		value += 1;
+		addValue = progressbar.val(value);
+
+		$('.progress-value').delay(200).html(value + '%');
+
+		if (value == max) {
+		    clearInterval(animate);
+		   	doneloading();   
+		}
+		};
+
+		var animate = setInterval(function() {
+		loading();
+		}, time);
+	});
+
+
+});
+
+
